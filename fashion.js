248d864c94ac7cc8e21e918ec1e1f66e1f26c3b4
Favour Onyeaghala <onyeaghalafavourr@gmail.com>
2022-06-30 13:37:20 +0100

    df

--- fashion.js
+++ fashion.js
@@ -1,1 +1,27 @@
+ var introText= document.querySelector('.intro-text');
+ var content= introText.textContent;
+ var split= content.split("")
+introText.textContent=""
+ for(let i=0;i<split.length;i++){
+ introText.innerHTML+="<span>"+split[i]+"</span>"
+ }
  
+let counter=0;
+ let timer=setInterval(tick,50);
+
+function tick(){
+    const span= introText.querySelectorAll("span")[counter];
+    span.classList.add('fade');
+    counter++;
+
+    if(counter===split.length){
+        complete();
+        return;
+    }
+
+}
+
+function complete(){
+    clearInterval(timer);
+    timer=null;
+}
